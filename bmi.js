5fde9fa22b27d9e499776924275a36a28b8e7f30
Nuellaachiaa <130145409+Nuellaachiaa@users.noreply.github.com>
2024-01-26 13:55:11 +0000

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,32 @@
+let Click = document.getElementById('click');
+let resultDiv = document.getElementById('resultDiv');
+let weightInput = document.getElementById('weightInput')
+let heightInput = document.getElementById('heightInput')
+let DisplayDiv = document.getElementById('DisplayDiv');
+
+
+// const BMI = (weightInput.value / (heightInput.value * heightInput.value)).toFixed(2);
+    const calculate = () => {
+    const weight = weightInput.value;
+    const height = heightInput.value;
+    const bmi = weight / (height * height);
+    
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0){
+        DisplayDiv.innerHTML = "Please input valid values"
+    } else if (bmi < 18.5) {
+        DisplayDiv.innerHTML = "You are in the underweight"
+    } else if (bmi > 24.9) {
+        DisplayDiv.innerHTML = "You are in the overweight"
+    } else {
+    DisplayDiv.innerHTML = "You are in the healthy range"
+    }
+
+        return bmi.toFixed(2);
+    }
+
+    Click.onclick = () => {
+        resultDiv.innerHTML = calculate();
+
+        
+}
+
