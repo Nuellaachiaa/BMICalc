bdeab9b92306d200cc58c883473c20e9b69cdf5f
Nuellaachiaa <130145409+Nuellaachiaa@users.noreply.github.com>
2024-05-06 19:50:22 +0000

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -1,32 +1,30 @@
-let Click = document.getElementById('click');
-let resultDiv = document.getElementById('resultDiv');
-let weightInput = document.getElementById('weightInput')
-let heightInput = document.getElementById('heightInput')
-let DisplayDiv = document.getElementById('DisplayDiv');
 
+var Click = document.getElementById('click');
+var resultDiv = document.getElementById('resultDiv');
+var weightInput = document.getElementById('weightInput');
+var heightInput = document.getElementById('heightInput');
+var DisplayDiv = document.getElementById('DisplayDiv');
 
-// const BMI = (weightInput.value / (heightInput.value * heightInput.value)).toFixed(2);
-    const calculate = () => {
-    const weight = weightInput.value;
-    const height = heightInput.value;
+const calculate = () => {
+    const weight = parseFloat(weightInput.value);
+    const height = parseFloat(heightInput.value);
     const bmi = weight / (height * height);
-    
-    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0){
-        DisplayDiv.innerHTML = "Please input valid values"
-    } else if (bmi < 18.5) {
-        DisplayDiv.innerHTML = "You are underweight"
-    } else if (bmi > 24.9) {
-        DisplayDiv.innerHTML = "You are overweight"
-    } else {
-    DisplayDiv.innerHTML = "You are in the healthy range"
-    }
 
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+        DisplayDiv.innerHTML = "Please input valid values";
+    } else {
+        const bmi = weight / (height * height);
+        if (bmi < 18.5) {
+            DisplayDiv.innerHTML = "You are underweight";
+        } else if (bmi > 24.9) {
+            DisplayDiv.innerHTML = "You are overweight";
+        } else {
+            DisplayDiv.innerHTML = "You are in the healthy range";
+        }
         return bmi.toFixed(2);
     }
-
-    Click.onclick = () => {
-        resultDiv.innerHTML = calculate();
-
-        
 }
 
+Click.onclick = () => {
+    resultDiv.innerHTML = calculate();
+}
